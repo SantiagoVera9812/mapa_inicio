e80087d82be3f56dd716f556139f55c507497a1f
SantiagoVera9812 <verasantiago981228@gmail.com>
2024-05-22 09:24:49 -0500

    mapa

---
+++ app/src/main/java/com/example/practica/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.practica
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.practica.fragments.MapsFragment
+import com.google.android.gms.maps.GoogleMap
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var fragment: MapsFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        fragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as MapsFragment
+
+
+    }
+}
